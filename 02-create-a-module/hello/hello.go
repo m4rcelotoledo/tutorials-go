7316220dc982713b32158c1e8babdf3c7a72f09b
Marcelo Toledo <marcelo.silva@arcadia.com>
2023-06-10 20:53:26 -0300

    Return greetings for multiple people - This topic introduced maps

--- 02-create-a-module/hello/hello.go
+++ 02-create-a-module/hello/hello.go
@@ -14,16 +14,20 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	// message, err := greetings.Hello("")
-	message, err := greetings.Hello("Marcelo")
+	// A slice of names.
+	names := []string{"Marcelo", "Caci", "Sif", "Frida"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names) // names
+	// message, err := greetings.Hello("") // empty name
+	// message, err := greetings.Hello("Marcelo") // only one name
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
