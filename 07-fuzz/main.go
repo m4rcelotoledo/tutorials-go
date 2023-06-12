59602ef22f0325a8c8e0e0a158b903d0afd00574
Marcelo Toledo <marcelo.silva@arcadia.com>
2023-06-12 18:21:35 -0300

    Fix the bug: byte -> rune

--- 07-fuzz/main.go
+++ 07-fuzz/main.go
@@ -11,10 +11,20 @@ func main() {
 	fmt.Printf("reversed again: %q\n", doubleRev)
 }
 
+// Reverse func with bug
+// func Reverse(s string) string {
+// 	b := []byte(s)
+// 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+// 		b[i], b[j] = b[j], b[i]
+// 	}
+// 	return string(b)
+// }
+
+// Fixed the error
 func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
 	}
-	return string(b)
+	return string(r)
 }
