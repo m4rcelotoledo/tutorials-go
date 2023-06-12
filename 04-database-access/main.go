6112682dcd03e76be8c702da750fcebb87eccd0f
Marcelo Toledo <marcelo.silva@arcadia.com>
2023-06-11 21:31:32 -0300

    Accessing a relational database

---
+++ 04-database-access/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		// User:   os.Getenv("DBUSER"), // 'nativeuser'
+		// Passwd: os.Getenv("DBPASS"), // 'password'
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
+}
