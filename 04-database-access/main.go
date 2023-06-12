d23a71a94527fe54ac9bdaba23fbb8f32647394e
Marcelo Toledo <marcelo.silva@arcadia.com>
2023-06-11 21:33:17 -0300

    Query for multiple rows

--- 04-database-access/main.go
+++ 04-database-access/main.go
@@ -42,4 +42,37 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+	albums, err := albumByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+// albumByArtist queries for albums that have the specified artist name.
+func albumByArtist(name string) ([]Album, error) {
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
 }
